eec38b3016b26b48d0334e66d1f97d21e813bae3
Salman Ahmad <salman.ahmad8@afiniti.com>
2019-07-12 21:42:32 +0500

    Running Typescript Express Sserver

--- Encryption/server.ts
+++ Encryption/server.ts
@@ -1,50 +1,101 @@
 import { RedisClient } from "redis";
 import express from "express";
-const bodyParser = require('body-parser');
-const redis = require('redis');
+import bodyParser = require('body-parser');
+import redis = require('redis');
 
 
 export class App {
 
     protected app: express.Application;
 
+    protected client: RedisClient = redis.createClient();
+
     constructor() {
         this.app = express();
         this.app.use(bodyParser.json());
 
+        console.log('Server');
 
-        this.app.listen(3000);
 
-   
-   this.app.post('/Decryption',(req,res,next)=>{
+        this.app.post('/Decryption', (req, res, next) => {
 
-    var EncryptArr = [];
-    var str:string = JSON.stringify(req.body.userdata, null, 2);
-    var retStr = this.Encryption(str);
-    res.send('Here is Decrypt Text:' + retStr);
+            let EncryptArr = [];
+            let str: string = JSON.stringify(req.body.userdata, null, 2);
+            let retStr = this.Encryption(str);
+            res.send(retStr);
 
-   })
-   
+        });
+
+
+
+
+        this.app.post('/Encryption', (req, res, next) => {
+            const userdata = req.body.userdata;
+            // Print redis errors to the console
+            this.client.on('error', (err) => {
+                console.log("Error " + err);
+            });
+
+            console.log('Quering cach Repo', userdata);
+            this.client.get(`afinitiWiki:${userdata}`, function (err, data) {
+                console.log('check Cach', data);
+
+                if (err) throw err;
+
+                if (data != null) {
+                    console.log('data', data);
+                    res.send(data);
+                } else {
+                    next();
+
+                }
+
+
+            });
+
+        }, (req, res) => {
+
+            console.log('Here in Next');
+            let str = req.body.userdata;
+            let retStr = this.Encryption(str);
+            this.client.setex(`afinitiWiki:${str}`, 3600, retStr);
+            res.send(retStr);
+
+        });
+
+
+
+
+
+
+
+
+
+
+        this.app.listen(3000, function () {
+            console.log('Example app listening on port 3000!');
+        });
+
     }
 
 
 
-     Encryption(str:string) { // LBH QVQ VG!
-        var convertedString = [];
+    Encryption(str: string) { // LBH QVQ VG!
+        var convertedString: string = "";
         var strLoc;
-        for(var i = 0; i < str.length; i++) {
-          strLoc = str.charCodeAt(i);
-          if(strLoc >=65 && strLoc < 78) {
-            convertedString.push(String.fromCharCode(strLoc + 13));
-          } else if (strLoc >= 78 && strLoc < 91) {
-            convertedString.push(String.fromCharCode(strLoc - 13));
-          } else {
-            convertedString.push(String.fromCharCode(strLoc));
-          }
+        for (var i = 0; i < str.length; i++) {
+            strLoc = str.charCodeAt(i);
+            if (strLoc >= 65 && strLoc < 78) {
+                convertedString = convertedString + String.fromCharCode(strLoc + 13);
+            } else if (strLoc >= 78 && strLoc < 91) {
+                convertedString = convertedString + String.fromCharCode(strLoc - 13);
+            } else {
+                convertedString = convertedString + String.fromCharCode(strLoc);
+            }
         }
+        console.log(convertedString);
 
-
-
-}
+        return convertedString;
+    }
 
 }
